997125f200992807d18e67df31c0c091f41aae24
Togrul Salmanli <togrulsalmanli33@gmail.com>
2021-02-01 22:22:01 +0400

    react

--- eurovacancy/src/Components/Header.js
+++ eurovacancy/src/Components/Header.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import HeaderBody from './HeaderBody'
 import HeaderFooter from './HeaderFooter'
-import Navbar from './Navbar'
-import '../Asests/Header.css'
+
 
 
 const Header = () => {
 	return (
 		<div className=" euro-header">
 			<div className="header-overlay"></div>
-			<Navbar />
 			<HeaderBody />
 			<HeaderFooter />
 		</div>
