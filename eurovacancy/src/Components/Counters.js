f92bf56fa81dad43aa41d070b0f6e23593452709
Togrul Salmanli <togrulsalmanli33@gmail.com>
2021-02-04 15:59:08 +0400

    react

---
+++ eurovacancy/src/Components/Counters.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+import CountUp from 'react-countup';
+import '../Asests/Counters.scss';
+
+const Counters = () => {
+
+	const [count] = useState([
+		{
+			euro_counters_items: "euro-counters-items",
+			euro_counters_item: "euro-counters-item",
+			countup: "countup",
+			end: 2500,
+			fa_plus: "fa fa-plus",
+			text: "Jobs Available"
+		},
+		{
+			euro_counters_items: "euro-counters-items",
+			euro_counters_item: "euro-counters-item",
+			countup: "countup",
+			end: 3500,
+			fa_plus: "fa fa-plus",
+			text: "Members"
+		},
+		{
+			euro_counters_items: "euro-counters-items",
+			euro_counters_item: "euro-counters-item",
+			countup: "countup",
+			end: 4500,
+			fa_plus: "fa fa-plus",
+			text: "Resumes"
+		},
+		{
+			euro_counters_items: "euro-counters-items",
+			euro_counters_item: "euro-counters-item",
+			countup: "countup",
+			end: 10500,
+			fa_plus: "fa fa-plus",
+			text: "Company"
+		}
+	])
+	return (
+		<>
+			<div className="container pt-0 pb-0 euro-counters">
+				<div className="row">
+					{
+						count.map(count => (
+							<div className="col-lg-3 counters-items">
+								<div className={count.euro_counters_item}>
+									<CountUp end={count.end} className={count.countup} />
+									<i className={count.fa_plus} aria-hidden="true"></i>
+								</div>
+								<p className="count-text">{count.text}</p>
+							</div>
+						))
+					}
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Counters
