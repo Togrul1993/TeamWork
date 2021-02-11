b50f383970cf3e3c66034da84f71062099ce1a78
Togrul Salmanli <togrulsalmanli33@gmail.com>
2021-02-11 22:18:07 +0400

    react

--- eurovacancy/src/Components/HeaderBody.js
+++ eurovacancy/src/Components/HeaderBody.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const HeaderBody = () => {
 
 	const onSubmit = (e) => {
-		e.preventDefault()
-	}
+		e.preventDefault();
+	};
 
 	return (
 		<>
